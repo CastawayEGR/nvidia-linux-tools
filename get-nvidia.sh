04ff41dd0672d643c48322d6fccf49abd89aa10c
Michael Tipton <36353334+CastawayEGR@users.noreply.github.com>
2021-10-17 13:27:59 -0400

    add some error checking

--- get-nvidia.sh
+++ get-nvidia.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 VERSION=$(curl -s https://raw.githubusercontent.com/aaronp24/nvidia-versions/master/nvidia-versions.txt | grep "current official" | cut -f 3 -d " ")
-echo "Downloading latest NVIDIA driver version: ${VERSION}"
-echo "  URL: http://us.download.nvidia.com/XFree86/Linux-x86_64/${VERSION}/NVIDIA-Linux-x86_64-${VERSION}.run"
-curl -o NVIDIA-Linux-x86_64-${VERSION}.run http://us.download.nvidia.com/XFree86/Linux-x86_64/${VERSION}/NVIDIA-Linux-x86_64-${VERSION}.run
-chmod +x NVIDIA-Linux-x86_64-${VERSION}.run
+FILE="NVIDIA-Linux-x86_64-${VERSION}.run"
+
+download_driver () {
+    if [ -f "${FILE}" ]; then
+        echo "${FILE} already exists..."
+    else
+        echo "Downloading latest NVIDIA driver version: ${VERSION}"
+        echo "  URL: http://us.download.nvidia.com/XFree86/Linux-x86_64/${VERSION}/${FILE}"
+        curl -o ${FILE} http://us.download.nvidia.com/XFree86/Linux-x86_64/${VERSION}/${FILE}
+        chmod +x ${FILE}
+        driver_check
+    fi
+}
+
+driver_check () {
+    FILECHECK=$(./${FILE} --check)
+    if [ "$FILECHECK" = "check sums and md5 sums are ok" ]; then
+        echo "${FILE} integrity verified!"
+    else
+        echo "Integrity check failed, removing and reattempting download..."
+        rm ${FILE}
+        download_driver
+    fi
+}
+
+download_driver
